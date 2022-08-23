ce3f193456cf367b7984a98f77488dec816bc4c9
sachieko <arashikabane@gmail.com>
2022-08-23 15:44:59 -0600

    Find Assist requests for teacher

---
+++ 4_queries/1_total_teacher_assist_requests.sql
@@ -0,0 +1,4 @@
+SELECT count(assistance_requests.id) as total_assistances, teachers.name AS name
+FROM assistance_requests JOIN teachers ON teachers.id = teacher_id 
+WHERE teachers.name = 'Waylon Boehm'
+GROUP BY teachers.name;
