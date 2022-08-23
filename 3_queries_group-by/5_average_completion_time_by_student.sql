bcb04d1484af199a208d4608a0449a4ec1b90ff7
sachieko <arashikabane@gmail.com>
2022-08-22 18:06:00 -0600

    Find average duration of assignment by student

---
+++ 3_queries_group-by/5_average_completion_time_by_student.sql
@@ -0,0 +1,5 @@
+SELECT students.name AS student, AVG(assignment_submissions.duration) as average_assignment_duration
+FROM students JOIN assignment_submissions ON students.id = assignment_submissions.student_id
+WHERE students.end_date IS NULL
+GROUP BY student
+ORDER BY average_assignment_duration DESC;
