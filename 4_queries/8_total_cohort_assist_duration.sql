6868dd41c0aa7d9dd5577bf39ea14514306ffb50
sachieko <arashikabane@gmail.com>
2022-08-23 17:40:22 -0600

    Add alias to cohorts.name

--- 4_queries/8_total_cohort_assist_duration.sql
+++ 4_queries/8_total_cohort_assist_duration.sql
@@ -1,4 +1,4 @@
-SELECT cohorts.name, SUM(completed_at - started_at) AS total_duration
+SELECT cohorts.name AS cohort, SUM(completed_at - started_at) AS total_duration
 FROM assistance_requests
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohorts.id = cohort_id
