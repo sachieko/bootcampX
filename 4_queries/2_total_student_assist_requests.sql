1d4c8e5c92ddeb64d06127db85bd28b26cc251f3
sachieko <arashikabane@gmail.com>
2022-08-23 15:54:53 -0600

    Remove redundant alias'

--- 4_queries/2_total_student_assist_requests.sql
+++ 4_queries/2_total_student_assist_requests.sql
@@ -1,4 +1,4 @@
-SELECT count(assistance_requests.id) as total_assistances, students.name AS name
+SELECT count(assistance_requests.id) as total_assistances, students.name
 FROM assistance_requests JOIN students ON students.id = student_id 
 WHERE students.name = 'Elliot Dickinson'
 GROUP BY students.name;
